9ad84388d7f06d7ad188043b693f5e1c8f37c4cf
Shyde <156099484+ShydeDev@users.noreply.github.com>
2024-06-09 21:43:22 +0200

    Update advancedCalculator.cpp
    Added modulo and factorial.
    Optimised code by moving class from a stack to a heap

--- advancedCalculator/advancedCalculator.cpp
+++ advancedCalculator/advancedCalculator.cpp
@@ -1,73 +1,80 @@
 // INCLUDES //
 #include <iostream>
+#include <iomanip>
 #include <variant>
+#include <string>
 #include <format>
 #include <functional>
-#include <cmath>
 
-constexpr char supported_operators[] = "+-*/^S"; // operators list
+// Operator lists
+constexpr char supported_operators[] = "+-*/^R%!";
+constexpr char single_operators[] = "R!";
 
-// Define an alias for type operation  
-using operation_t = std::variant<std::plus<>, std::minus<>, std::multiplies<>, std::function<double(double, double)>>; //  using variant to  store different operation types
+// Define an alias for type operation using variant
+using operation_t = std::variant<std::plus<>, std::minus<>, std::multiplies<>, std::function<double(double, double)>>;
 
-// Define a function to get the operation object based on the input
-operation_t get_operation(char op) {
-    switch (op) {
-    case '+': return std::plus<>(); // addition op
-    case '-': return std::minus<>(); // subtraction op
-    case '*': return std::multiplies<>(); // multiplication op
-    case '/': return [](double num1, double num2) { // division func
-        if (num2 == 0) {
-            throw std::invalid_argument("Division by zero");
-        }
-        return num1 / num2;
-        };
-    case 'S': return [](double num1, double _) { // square root func
-        if (num1 < 0) {
-            throw std::invalid_argument("Cannot take a square root of negative number");
-        }
-        return std::sqrt(num1);
-        };
-    case '^': return [](double num1, double num2) { return std::pow(num1, num2); };
+// Create a calculator class
+class Calculator {
+public:
+	operation_t get_operator(char op) {
+		switch (op)
+		{
+		case '+': return std::plus<>(); // addition op
+		case '-': return std::minus<>(); // substraction op
+		case '*': return std::multiplies<>(); // multiplication op
+		case '^': return [](double num1, double num2) {return std::pow(num1, num2); }; // pow op
+		case '/': return [](double num1, double num2) { return num2 == 0 ? throw std::invalid_argument("Division by 0") : num1 / num2;  }; // division op
+		case 'R': return [](double num1, double _) {return num1 < 0 ? throw std::invalid_argument("Cannot find a square root of a negative number!") : std::sqrt(num1); }; // square root op
+		case '%': return [](double num1, double num2) {return num2 == 0 ? throw std::invalid_argument("Division by zero") : static_cast<int>(num1) % static_cast<int>(num2); }; // modulo op
+		case '!': return [](double num1, double _) { if (num1 < 0) throw std::invalid_argument("Negative number"); std::function<double(double)> fact = [&](double n) -> double {return (n <= 1) ? 1 : n * fact(n - 1);};return fact(num1);}; // factorial op
+		default: throw std::invalid_argument("Invalid operator"); // throw an error about unsupported operator
+		}
+	}
 
-    default: throw std::invalid_argument("Invalid operator"); // throw unsupported op
-    }
-}
-
-// Define function to do the calculation using operation object
-double calculate(double num1, double num2, const operation_t& op) {
-    // using visit to call appropriate function based on the operator
-    return std::visit([&](auto f) { return f(num1, num2); }, op);
-}
+	inline double calculate(double num1, double num2, operation_t op) {
+		return std::visit([&](auto f) {return f(num1, num2); }, op); // using std::visit to call appropriate function based on the operator
+	}
+};
 
 int main() {
-    char op;
-    double num1, num2;
+	char op;
+	double num1, num2;
+	bool singleOp = false;
 
-    // loop until user enters a valid operator
-    while (strchr(supported_operators, op) == nullptr) {
-        std::cout << "Enter operator (" << supported_operators << ")" << " (- 'S' means square root): ";
-        std::cin >> op;
-    }
+	// loop until user enters a valid operator
+	while (strchr(supported_operators, op) == nullptr) {
+		std::cout << "Enter an operator (";
 
-    // Add square root exclusion
-    if (op != 'S') {
-        std::cout << "Enter two numbers: ";
-        std::cin >> num1 >> num2;
-    } else {
-        std::cout << "Enter a number: ";
-        std::cin >> num1;
-        num2 = 0; // set num2 to 0 cuz you dont need a second number in square rooting
-    }
+		for (uint8_t i = 0; i < sizeof(supported_operators) - 2; ++i) {
+			std::cout << supported_operators[i] << ", ";
+		}
+		std::cout << supported_operators[sizeof(supported_operators) - 2] << "): ";
 
-    // Handle output
-    try {
-        const auto operation = get_operation(op);
-        const auto result = calculate(num1, num2, operation);
-        const auto formatted = std::format("{} {} {} = {}\n", num1, op, num2, result);
+		std::cin >> op;
+	}
 
-        std::cout << formatted;
-    } catch (const std::exception& e) {
-        std::cout << "Error: " << e.what() << "\n";
-    }
+	// Making an one number for factorial and square root
+	if (!strchr(single_operators, op)) {
+		std::cout << "Enter two numbers: ";
+		std::cin >> num1 >> num2;
+	} else {
+		std::cout << "Enter a number: ";
+		std::cin >> num1;
+		num2 = 0;
+		singleOp = true;
+	}
+
+	// Handle output
+	try {
+		std::unique_ptr<Calculator> calculator = std::make_unique<Calculator>(); // moving class from stack to heap for better optimisation
+		const operation_t operation = calculator->get_operator(op);
+		const double result = calculator->calculate(num1, num2, operation);
+		const std::string formatted = !singleOp ? std::format("{} {} {} = {}", num1, op, num2, result) : std::format("{} = {}", num1, result);
+
+		std::cout << formatted;
+	}
+	// Throw an error
+	catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << "\n";
+	}
 }
